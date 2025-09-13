fbec70ecce2786de083b7294227ec405c73435d3
Subhamjyoti Rana <subhamjyotiranagcsj@gmail.com>
2025-09-13 08:24:17 +0530

    Update app.ts
    Updating ts file

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,18 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('counter-app');
+  count: number = 0
+
+  handelIncrement() {
+    this.count = this.count + 1
+  }
+  handelDecrement() {
+    if(this.count>0){
+      this.count = this.count - 1
+    }
+    
+  }
+  handelReset() {
+    this.count = 0
+  }
 }
