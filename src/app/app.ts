22e6f07a9816259dc51a4d7170d085e4bc8b9208
Subhamjyoti Rana <subhamjyotiranagcsj@gmail.com>
2025-09-29 07:54:03 +0530

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,47 +3,24 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrls: ['./app.css']
+  styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('counter-app');
-  count: number = 0;
-
-  // Online audio sources
-  incrementSound = new Audio('https://www.myinstants.com/media/sounds/mouse-click.mp3');
-  decrementSound = new Audio('https://www.myinstants.com/media/sounds/click_x.wav');
-  resetSound = new Audio('https://www.myinstants.com/media/sounds/arcade-retro.mp3');
-
-  constructor() {
-    this.incrementSound.load();
-    this.decrementSound.load();
-    this.resetSound.load();
-  }
+  count: number = 0
 
   handelIncrement() {
-    this.count++;
-    this.playSound(this.incrementSound);
+    this.count = this.count + 1
   }
-
   handelDecrement() {
-    if (this.count > 0) {
-      this.count--;
-      this.playSound(this.decrementSound);
+    if(this.count>0){
+      this.count = this.count - 1
     }
+    
   }
-
   handelReset() {
-    this.count = 0;
-    this.playSound(this.resetSound);
-  }
-
-  playSound(sound: HTMLAudioElement) {
-    sound.currentTime = 0;
-    sound.play().catch(err => {
-      console.error('Audio playback failed:', err);
-    });
+    this.count = 0
   }
 }
